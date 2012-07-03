10949c963553b0db198f3ee9a18b072de8a53f66
Trond Bjerkestrand <trond.bjerkestrand@typesafe.com>
2012-07-03 15:53:36 +0200

    Some changes.

--- src/main/scala/com/groosker/api/package.scala
+++ src/main/scala/com/groosker/api/package.scala
@@ -2,49 +2,57 @@ package com.groosker
 
 package object api {
 
+  abstract class Params {
+    val params = commonParams ++ callParams
+    val callParams: List[String]
+    val result: List[String]
+    val commonParams = List("api_key", "version")
+    val List(apiKey, version) = commonParams
+  }
   abstract class ApiUrl {
-    abstract class Params {
-      val params: List[String]
-      val commonParams = List("api_key", "version")
-      val List(apiKey, version) = commonParams
-    }
     def url: String
     def params: Params
+    //    def unapply(x: Any) = x match {
+    //      case r @ Req(List("api", `url`), _, PostRequest) => Some(r, r.paramNames.toSet)
+    //      case _ => None
+    //    }
+
   }
 
   object RequestPayment extends ApiUrl {
     val url = "request_payment"
     object params extends Params {
-      val params = List("amount", "currency", "receiver", "details")
+      val callParams = List("amount", "currency", "receiver", "details")
       val List(amount, currency, receiver, details) = params
+      val result = List("code", "url")
+      val List(code, url) = result
     }
   }
 
   object AwaitPayment extends ApiUrl {
     val url = "await_payment"
     object params extends Params {
-      val params = List("code")
+      val callParams = List("code")
       val (code) = params
+      val result = List("result")
     }
   }
 
   object AcceptTestPayment extends ApiUrl {
     val url = "accept_test_payment"
     object params extends Params {
-      val params = List("code")
+      val callParams = List("code")
       val (code) = params
+      val result = List("result")
     }
   }
 
   object DeclineTestPayment extends ApiUrl {
     val url = "decline_test_payment"
     object params extends Params {
-      val params = List("code")
+      val callParams = List("code")
       val (code) = params
+      val result = List("result")
     }
   }
-
-  private val upayUrls = List("requestPayment", "generateQRCode", "awaitPayment", "acceptTestPayment", "declineTestPayment") map { url => List("api", url) }
-  val List(requestPaymentUrl, generateQRCodeUrl, awaitPaymentUrl, acceptTestPaymentUrl, declineTestPaymentUrl) = upayUrls
-
 }
